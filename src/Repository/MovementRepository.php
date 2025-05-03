d4a61e580756a2de125b85e5596ddfa587ac9318
Marius1code <kanzemariuscode@gmail.com>
2025-05-03 03:44:26 +0100

    Add src/Repository/MovementRepository.php 1746240265710

---
+++ src/Repository/MovementRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Movement;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Movement>
+ *
+ * @method Movement|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Movement|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Movement[]    findAll()
+ * @method Movement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MovementRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Movement::class);
+    }
+}
